4c59fa84f2b81ed2aac8e8cc502462c2cf89b301
bilhares <felipe.bilhares@hotmail.com>
2020-05-13 13:53:37 -0400

    create new seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _service.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller s)
+        {
+            _service.Insert(s);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
