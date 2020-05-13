a1da625f0d09e6037617c700e978c74229f84a3f
bilhares <felipe.bilhares@hotmail.com>
2020-05-13 13:37:02 -0400

    Sellers service e buscar todos

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private SellerService _service;
+
+        public SellersController(SellerService service)
+        {
+            this._service = service;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _service.FindAll();
+            return View(list);
         }
     }
 }
