f5afcb3d0c77e7c1083403438bb63c5726d93ace
bilhares <felipe.bilhares@hotmail.com>
2020-05-13 14:05:57 -0400

    integridade referencial

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller s)
         {
+            s.Department = _context.Department.First();
             _context.Add(s);
             _context.SaveChanges();
         }
