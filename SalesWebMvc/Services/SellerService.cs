9b2015a0a7fd68998a68f1fd1dab517f046122bb
bilhares <felipe.bilhares@hotmail.com>
2020-05-14 13:28:15 -0400

    tratando erro ao deletar

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,8 +1,6 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Services.Exceptions;
@@ -37,8 +35,14 @@ namespace SalesWebMvc.Services
         public async Task RemoveAsync(int id)
         {
             var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Cant delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller seller)
